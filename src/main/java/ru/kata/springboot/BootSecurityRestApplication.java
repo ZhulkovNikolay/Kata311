4d53682f6aada13bc2f808f86256d69047e0dfcf
ZhulkovNikolay <wed10@yandex.ru>
2025-04-08 20:17:01 +0300

    firstCommit

---
+++ src/main/java/ru/kata/springboot/BootSecurityRestApplication.java
@@ -0,0 +1,14 @@
+package ru.kata.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSecurityRestApplication.class, args);
+	}
+	//firstCommit
+
+}
